8d654f18e915622e9728328777ae3a4618a4adf3
chpatola <charlotte.patola@helsinki.fi>
2021-09-08 10:02:49 +0300

    Added basic project setup

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #Supress warning
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
